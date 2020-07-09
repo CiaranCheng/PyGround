3fb0d4e5cbb99472d4ac2d66b575e47dbf1b0b77
CiaranCheng <1095074672@qq.com>
2020-07-09 14:11:23 +0800

    [Del]删除多余项目

--- print.py
+++
@@ -1,4 +0,0 @@
-        
-        # 先根据运行时设置进行过滤，然后再判断打印控制
-        res_ids = [rec['id'] for rec in print_data[self.env.get(res_model)._table]]
-        self.env['print.control'].validation(res_model, res_ids)
